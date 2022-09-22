01afc299ea8feb219093159f6b97fe6f56402df3
AnastasiiaFaleeva <113059067+AnastasiiaFaleeva@users.noreply.github.com>
2022-09-22 06:59:50 +0300

    Задача 16

--- task16/Program.cs
+++ task16/Program.cs
@@ -5,3 +5,16 @@
 // -4, 16 -> да
 // 25, 5 -> да
 // 8,9 -> нет
+
+bool Multip(int num1, int num2)
+{
+    return num1 % num2 == 0 || num2 % num1 == 0;
+}
+
+Console.WriteLine("Введите первое число ");
+int num1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите второе число ");
+int num2 = Convert.ToInt32(Console.ReadLine());
+
+if (Multip(num1, num2)) Console.WriteLine($"{num1}, {num2} -> да");
+else Console.WriteLine($"{num1}, {num2} -> нет");
