6c032f62449e613481767dcec1eb2eeba7cb1496
AnastasiiaFaleeva <113059067+AnastasiiaFaleeva@users.noreply.github.com>
2022-09-22 10:27:47 +0300

    Задача 13

--- HomeWork_task13/Program.cs
+++ HomeWork_task13/Program.cs
@@ -1,2 +1,28 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
+
+// 645 -> 5
+
+// 78 -> третьей цифры нет
+
+// 32679 -> 6
+
+
+int ThirdDigit(int num)
+{
+    while (num / 1000 > 1)
+    {
+        int num1 = num / 10;
+        num = num1;
+    };
+    int n = num % 10;
+    return n;
+}
+
+Console.WriteLine("Введите число ");
+int number = Convert.ToInt32(Console.ReadLine());
+if ((number / 100) > 1)
+{
+    int result = ThirdDigit(number);
+    Console.Write($"{number} -> {result} ");
+}
+else Console.Write($"{number} -> третьей цифры нет");
