238fb70851d99e6333e3c84823259fcf3ea145e5
AnastasiiaFaleeva <113059067+AnastasiiaFaleeva@users.noreply.github.com>
2022-09-22 09:14:29 +0300

    Задача 15

--- HomeWork_task15/Program.cs
+++ HomeWork_task15/Program.cs
@@ -1,2 +1,21 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Напишите программу, которая принимает на вход цифру, обозначающую день недели, и проверяет, является ли этот день выходным.
+
+// 6 -> да
+// 7 -> да
+// 1 -> нет
+
+
+bool Week(int num)
+{
+    return num == 6 || num == 7;
+}
+
+Console.WriteLine("Введите число, которое соответствует дню недели ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+if (number <= 7)
+{
+    if (Week(number)) Console.WriteLine($"{number} -> да");
+    else Console.WriteLine($"{number} -> нет");
+}
+else Console.WriteLine($"число {number} не является числом дня недели");
