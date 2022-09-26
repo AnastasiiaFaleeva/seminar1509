9bf183ae9a4afbcdc6e172041c327cfe7b431bdc
AnastasiiaFaleeva <113059067+AnastasiiaFaleeva@users.noreply.github.com>
2022-09-26 15:06:39 +0300

    Задача 23

--- HomeWork_task23/Program.cs
+++ HomeWork_task23/Program.cs
@@ -2,3 +2,18 @@
 // Напишите программу, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N.
 // 3 -> 1, 8, 27
 // 5 -> 1, 8, 27, 64, 125
+
+Console.WriteLine("Введите число ");
+int num = Convert.ToInt32(Console.ReadLine()); 
+
+void Cub(int number)
+{
+    int counter = 1;
+    while(counter <= number)
+    {
+        Console.WriteLine($" |{counter,2}|  |{counter * counter * counter,4}| ");
+        counter++;
+    }
+}
+if(num>1) Cub(num);
+else Console.WriteLine("Введено некоректное значение ");
