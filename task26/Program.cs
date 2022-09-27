504db52ee03cd738b430af16f3aab4c8c69e4079
AnastasiiaFaleeva <113059067+AnastasiiaFaleeva@users.noreply.github.com>
2022-09-27 11:14:19 +0300

    Решение

---
+++ task26/Program.cs
@@ -0,0 +1,24 @@
+﻿// Задача 26: Напишите программу, которая принимает
+// на вход число и выдаёт количество цифр в числе.
+// 456 -> 3
+// 78 -> 2
+// 89126 -> 5
+
+Console.WriteLine("Введите число ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+int Sam(int num)
+{
+    int counter = 0;
+    int res = 0;
+    while (num >= 0)
+    {
+        num = num / 10;
+        res = res+1;
+        counter++;
+    }
+    return res;
+}
+
+int result = Sam(Math.Abs(number));
+Console.WriteLine($"{number} -> {result}");
