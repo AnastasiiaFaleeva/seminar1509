3c12a4737d2d8a65bde8afd76b4f01c712c94e8b
AnastasiiaFaleeva <113059067+AnastasiiaFaleeva@users.noreply.github.com>
2022-09-28 11:30:48 +0300

    Домашние задачи 4 семинар

--- HomeWork_task27/Program.cs
+++ HomeWork_task27/Program.cs
@@ -2,3 +2,20 @@
 // 452 -> 11
 // 82 -> 10
 // 9012 -> 12
+
+Console.WriteLine("Введите число ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+int Qalty(int num)
+{
+    int sum = 0;
+    while (num > 0)
+    {
+        sum = sum + num % 10;
+        num = num / 10;
+    }
+    return sum;
+}
+
+int result = Qalty(number);
+Console.WriteLine($"{number} -> {result}");
