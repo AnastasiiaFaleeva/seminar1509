42d9179ce70dce22319f29ca7424ce5f3f2f669d
AnastasiiaFaleeva <113059067+AnastasiiaFaleeva@users.noreply.github.com>
2022-10-06 10:33:01 +0300

    задача 35

---
+++ task33/Program.cs
@@ -0,0 +1,53 @@
+﻿// Задача 33: Задайте массив. Напишите программу, которая
+// определяет, присутствует ли заданное число в массиве.
+// 4; массив [6, 7, 19, 345, 3] -> нет
+// 3; массив [6, 7, 19, 345, 3] -> да
+
+
+Console.WriteLine("Введите заданное число ");
+int number = Convert.ToInt32(System.Console.ReadLine());
+
+
+int[] CreateArrayRndInt(int size, int min, int max)    // МЕТОД ДЛЯ СОЗДАНИЯ МАССИВА ИЗ СЛУЧАЙНЫХ ЧИСЕЛ
+{
+    int[] array = new int[size];
+    var rnd = new Random();    // Создали переменную для случайных чисел
+
+    for (int i = 0; i < size; i++)
+    {
+        array[i] = rnd.Next(min, max + 1);
+    }
+    return array;
+}
+
+void PrintArray(int[] array)
+{
+    Console.Write("[");
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (i < array.Length - 1) Console.Write($"{array[i]}, ");
+        else Console.Write($"{array[i]}");
+    }
+    Console.WriteLine("]");
+}
+
+bool ChekNumber(int[] array, int arg)
+{
+    bool chekNumber = false;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i] == arg)
+        {
+            chekNumber = true;
+            break;
+        }
+    }
+    return chekNumber;
+}
+
+int[] arr = CreateArrayRndInt(12, 0, 20);
+PrintArray(arr);
+bool chekNumber = ChekNumber(arr, number);
+
+if (chekNumber) Console.WriteLine("Yes");
+else Console.WriteLine("No");
---
+++ task37/Program.cs
@@ -0,0 +1,28 @@
+﻿// Задача 37: Найдите произведение пар чисел в одномерном массиве.
+// Парой считаем первый и последний элемент, второй и предпоследний
+// и т.д. Результат запишите в новом массиве.
+// [1 2 3 4 5] -> 5 8 3
+// [6 7 3 6] -> 36 21
+
+int[] CreateArrayRndInt(int size, int min, int max)    // МЕТОД ДЛЯ СОЗДАНИЯ МАССИВА ИЗ СЛУЧАЙНЫХ ЧИСЕЛ
+{
+    int[] array = new int[size];
+    var rnd = new Random();    // Создали переменную для случайных чисел
+
+    for (int i = 0; i < size; i++)
+    {
+        array[i] = rnd.Next(min, max + 1);
+    }
+    return array;
+}
+
+void PrintArray(int[] array) // МЕТОД ДЛЯ ПЕЧАТИ МАССИВА ИЗ СЛУЧАЙНЫХ ЧИСЕЛ
+{
+    Console.Write("[");
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (i < array.Length - 1) Console.Write($"{array[i]}, ");
+        else Console.Write($"{array[i]}");
+    }
+    Console.WriteLine("]");
+}
