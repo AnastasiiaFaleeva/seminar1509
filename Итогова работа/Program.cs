f9f00fcebe6708c228b2a6f5d21a44f2b6b4cb2d
AnastasiiaFaleeva <113059067+AnastasiiaFaleeva@users.noreply.github.com>
2022-10-27 16:04:48 +0300

    Задача 65

--- Итогова работа/Program.cs
+++ Итогова работа/Program.cs
@@ -1,81 +1,73 @@
-﻿// Задача 54: Задайте двумерный массив. 
-// Напишите программу, которая упорядочит по убыванию элементы каждой строки двумерного массива.
-// Например, задан массив:
-// 1 4 7 2
-// 5 9 2 3
-// 8 4 2 4
-// В итоге получается вот такой массив:
-// 7 4 2 1
-// 9 5 3 2
-// 8 4 4 2
-int[,] array2D = CreateMatrixRndInt(3, 4, 1, 9);
-PrintMatrix(array2D);
-int[] arrEx = new int[] {1, 2, 5, 8, 4};
-BubbleSort(arrEx);
-
-Console.WriteLine();
-FromHiToLowInRow(array2D);
-PrintMatrix(array2D);
-
-void FromHiToLowInRow(int[,] matrix)
+﻿string[] baseArray = new string[]
 {
-    for (int i = 0; i < matrix.GetLength(0); i++)
+    "Самара", "Омск", "Казань", "Челябинск", "Ростов-на-Дону", "Уфа",
+    "Волгоград", "Пермь", "арм", "бел", "боп", "газ", "дук", "ева",
+    "зов", "орс", "чаж", "эра", "Красноярск", "Воронеж", "Саратов",
+    "Краснодар", "Тольятти", "Ижевск", "Ульяновск", "Барнаул",
+    "Владивосток", "бег", "гад", "ира", "лях", "мит", "мод", "сад",
+    "уок", "хек", "циф", "аш", "ло", "но", "од", "он", "ра", "ре",
+    "хи", "цы", "ял", "Томск", "Пенза", "Липецк", "Тула", "Киров",
+    "Брянск", "Курск", "Иваново", "Сочи", "Курган", "Смоленск"
+};
+
+string[] CreateArray(int count)
+{
+    return new string[count];
+}
+
+void FillArray(string[] array, string[] basic, int min, int max)
+{
+    int length = array.Length;
+    Random randomElement = new Random();
+    for (int i = 0; i < length; i++)
     {
-        int[] rowArr = new int[matrix.GetLength(1)];
-        for (int j = 0; j < matrix.GetLength(1); j++)
-        {
-            rowArr[j] = matrix[i, j];
-        }
-        BubbleSort(rowArr);
-        for (int k = 0; k < matrix.GetLength(1); k++)
-        {
-            matrix[i, k] = rowArr[k];
-        }
+        array[i] = basic[randomElement.Next(min, max)];
     }
 }
 
-int[] BubbleSort(int[] arr)
+string[] SolutionMainTask(string[] createdArray)
 {
-    int temp;
-    for (int i = 0; i < arr.Length - 1; i++)
+    int lengthOutputArray = 0;
+    int lengthCreatedArray = createdArray.Length;
+
+    for (int i = 0; i < lengthCreatedArray; i++)
     {
-        for (int j = 0; j < arr.Length - i - 1; j++)
+        if (createdArray[i].Length <= 3) lengthOutputArray++;
+    }
+
+    string[] outputArray = new string[lengthOutputArray];
+
+    int index = 0;
+    for (int j = 0; j < lengthCreatedArray; j++)
+    {
+        if (createdArray[j].Length <= 3)
         {
-            if (arr[j + 1] > arr[j])
-            {
-                temp = arr[j + 1];
-                arr[j + 1] = arr[j];
-                arr[j] = temp;
-            }
+            outputArray[index] = createdArray[j];
+            index++;
         }
     }
-    return arr;
+
+    return outputArray;
 }
 
-int[,] CreateMatrixRndInt(int rows, int columns, int min, int max)
+string PrintArray(string[] array)
 {
-    int[,] matrix = new int[rows, columns];
-    var rnd = new Random();
-    for (int i = 0; i < matrix.GetLength(0); i++)
+    string result = "[";
+    int lengthArray = array.Length;
+    for (int i = 0; i < lengthArray; i++)
     {
-        for (int j = 0; j < matrix.GetLength(1); j++)
-        {
-            matrix[i, j] = rnd.Next(min, max + 1);
-        }
+        result += $"{array[i]}, ";
     }
-    return matrix;
+
+    if (lengthArray == 0) result += "00";
+    int lengthResult = result.Length;
+    result = result.Remove(lengthResult - 2, 2);
+    result += "]";
+    return result;
 }
 
-void PrintMatrix(int[,] matrix)
-{
-    for (int i = 0; i < matrix.GetLength(0); i++)
-    {
-        Console.Write("|");
-        for (int j = 0; j < matrix.GetLength(1); j++)
-        {
-            if (j < matrix.GetLength(1) - 1) Console.Write($"{matrix[i, j],4}, ");
-            else Console.Write($"{matrix[i, j],4}");
-        }
-        Console.WriteLine("   |");
-    }
-}
+var createdArray = CreateArray(5);
+FillArray(createdArray, baseArray, 1, baseArray.Length);
+var outputArray = SolutionMainTask(createdArray);
+Console.WriteLine($"Основной массив -> {PrintArray(createdArray)}");
+Console.WriteLine($"Сформированный массив -> {PrintArray(outputArray)}");
